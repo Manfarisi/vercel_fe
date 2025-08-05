692dce89b6047bd2cd7d5b734042410acbd24bc7
Salman <kingmansalman@gmail.com>
2025-08-05 13:40:53 +0700

    update

--- src/pages/TambahProduk/Add.jsx
+++ src/pages/TambahProduk/Add.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { assets } from "../../assets/assets";
 import axios from "axios";
-import { toast } from "react-toastify";
 import {
   FaBoxOpen,
   FaMoneyBillWave,
@@ -72,6 +71,8 @@ const Add = ({ url }) => {
           harga: "",
           kategori: "",
           hpp: "",
+          kodeAngka: "",
+          idProduk: "",
         });
         setImage(false);
 
